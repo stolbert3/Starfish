4518db0c5b58a2ffaa17fcf85f0ee6329b20b590
Sydney <stolbert3@gatech.edu>
2019-01-19 12:21:48 -0500

    added images to default tasks

---
+++ scripts/defaultTasksDB.js
@@ -0,0 +1,96 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/reactreadinglist"
+);
+
+const defaultTaskSeed = [
+  {
+    taskId: 1,
+    name: "Brush Teeth",
+    image: "../mulberry-symbols/EN-symbols/brush_teeth_,_to.svg",
+    complete: false
+  },
+  {
+    taskId: 2,
+    name: "Brush Hair",
+    image: "../mulberry-symbols/EN-symbols/brush_hair_,_to.svg",
+    complete: false
+  },
+  {
+    taskId: 3,
+    name: "Get Dressed",
+    image: "../mulberry-symbols/EN-symbols/change_clothes_,_to.svg",
+    complete: false
+  },
+  {
+    taskId: 4,
+    name: "Eat Breakfast",
+    image: "../mulberry-symbols/EN-symbols/cornflakes.svg",
+    complete: false
+  },
+  {
+    taskId: 5,
+    name: "Eat Lunch",
+    image: "../mulberry-symbols/EN-symbols/sandwich.svg",
+    complete: false
+  },
+  {
+    taskId: 6,
+    name: "Eat Dinner",
+    image: "../mulberry-symbols/EN-symbols/dinner.svg",
+    complete: false
+  },
+  {
+    taskId: 7,
+    name: "Play Inside",
+    image: "../mulberry-symbols/EN-symbols/play_,_to.svg",
+    complete: false
+  },
+  {
+    taskId: 8,
+    name: "Play Oustide",
+    image: "../mulberry-symbols/EN-symbols/swing_,_to.svg",
+    complete: false
+  },
+  {
+    taskId: 9,
+    name: "In-home Therapy",
+    image: "../mulberry-symbols/EN-symbols/speech_language_therapist_1a.svg",
+    complete: false
+  },
+  {
+    taskId: 10,
+    name: "Take Shower",
+    image: "../mulberry-symbols/EN-symbols/shower_1_,_to.svg",
+    complete: false
+  },
+  {
+    taskId: 11,
+    name: "Put on PJs",
+    image: "../mulberry-symbols/EN-symbols/pyjamas.svg",
+    complete: false
+  },
+  {
+    taskId: 12,
+    name: "Go to Bed",
+    image: "../mulberry-symbols/EN-symbols/sleep_male_,_to.svg",
+    complete: false
+  }
+]
+
+db.User
+  .remove({})
+  .then(() => db.User.collection.insertMany(defaultTaskSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
