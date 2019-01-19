3fe96cff5d0a7a13852c657e720ac1c417553d52
Scott Ritchey <scottritchey@Scotts-MacBook-Air-2.local>
2019-01-19 11:39:20 -0500

    auth update fix

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,3 +1,9 @@
+import  React from "react";
+import { Route, Router } from "react-router-dom";
+import Home from './Home';
+import Callback from './Callback';
+import Auth from './auth';
+import history from './history';
 const auth = new Auth();
 
 const handleAuthentication = (nextState, replace) => {
