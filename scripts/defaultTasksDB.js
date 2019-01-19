4994972cf0798d969fff82cf1003bbeda29cfd80
Sydney <stolbert3@gatech.edu>
2019-01-19 11:54:07 -0500

    schema work

--- scripts/defaultTasksDB.js
+++ scripts/defaultTasksDB.js
@@ -12,73 +12,73 @@ const defaultTaskSeed = [
   {
     taskId: 1,
     name: "Brush Teeth",
-    image: "imagelink",
+    image: "../mulberry-symbols/EN-symbols/brush_teeth_,_to.svg",
     complete: false
   },
   {
     taskId: 2,
-    name: "Get Dressed",
-    image: "imagelink",
+    name: "Brush Hair",
+    image: "../mulberry-symbols/EN-symbols/brush_hair_,_to.svg",
     complete: false
   },
   {
     taskId: 3,
-    name: "Eat Breakfast",
-    image: "imagelink",
+    name: "Get Dressed",
+    image: "../mulberry-symbols/EN-symbols/change_clothes_,_to.svg",
     complete: false
   },
   {
     taskId: 4,
-    name: "Eat Lunch",
-    image: "imagelink",
+    name: "Eat Breakfast",
+    image: "../mulberry-symbols/EN-symbols/",
     complete: false
   },
   {
     taskId: 5,
-    name: "Eat Dinner",
-    image: "imagelink",
+    name: "Eat Lunch",
+    image: "../mulberry-symbols/EN-symbols/",
     complete: false
   },
   {
     taskId: 6,
-    name: "Eat Snack",
-    image: "imagelink",
+    name: "Eat Dinner",
+    image: "../mulberry-symbols/EN-symbols/",
     complete: false
   },
   {
     taskId: 7,
     name: "Play Inside",
-    image: "imagelink",
+    image: "../mulberry-symbols/EN-symbols/",
     complete: false
   },
   {
     taskId: 8,
     name: "Play Oustide",
-    image: "imagelink",
+    image: "../mulberry-symbols/EN-symbols/",
     complete: false
   },
   {
     taskId: 9,
     name: "In-home Therapy",
-    image: "imagelink",
+    image: "../mulberry-symbols/EN-symbols/",
     complete: false
   },
   {
     taskId: 10,
     name: "Take Bath",
-    image: "imagelink",
+    image: "../mulberry-symbols/EN-symbols/",
     complete: false
   },
   {
     taskId: 11,
     name: "Put on PJs",
-    image: "imagelink",
+    image: "../mulberry-symbols/EN-symbols/",
     complete: false
   },
   {
     taskId: 12,
     name: "Go to Bed",
-    image: "imagelink",
+    image: "../mulberry-symbols/EN-symbols/",
     complete: false
   }
 ]
