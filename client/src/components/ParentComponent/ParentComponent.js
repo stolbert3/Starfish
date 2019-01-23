d3c87dabcb1d6153d8c0f546fbaa9619fcdaf763
Abdul Hameed Sabree <ahs4448@yahoo.com>
2019-01-22 20:24:36 -0500

    updates to login page etc

---
+++ client/src/components/ParentComponent/ParentComponent.js
@@ -0,0 +1,39 @@
+import  React  from 'react';
+
+class ParentComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+      return(
+
+        <div class="container">
+        <h1> Name </h1>
+
+        <img src="..." alt="..." class="img-thumbnail" />
+
+        <hr />
+
+        <div>
+            <input type="checkbox" name="Task1" value="Task1" />
+            Task 1
+            <img src="" alt="" />
+        </div>
+        <div>
+            <input type="checkbox" name="Task1" value="Task1" />
+            Task 2
+        </div>
+
+        <div>
+            <input type="checkbox" name="Task1" value="Task1" />
+            Task 3
+        </div>
+
+        <div>
+            <input type="checkbox" name="Task1" value="Task1" />
+            Task 4
+        </div>
+        </div>
+      );
+    }
+}
