e2f5ea3a9f8f5f86f4d3b01dbc9b9d4df1d93520
Sydney <stolbert3@gatech.edu>
2019-01-31 19:31:12 -0500

    default user seeds file added in scripts

--- scripts/defaultTasksDB.js
+++ scripts/defaultTasksDB.js
@@ -11,73 +11,85 @@ const defaultTaskSeed = [
     taskId: 1,
     name: "Brush Teeth",
     image: "../mulberry-symbols/EN-symbols/brush_teeth_,_to.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 2,
     name: "Brush Hair",
     image: "../mulberry-symbols/EN-symbols/brush_hair_,_to.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 3,
     name: "Get Dressed",
     image: "../mulberry-symbols/EN-symbols/change_clothes_,_to.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 4,
     name: "Eat Breakfast",
     image: "../mulberry-symbols/EN-symbols/cornflakes.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 5,
     name: "Eat Lunch",
     image: "../mulberry-symbols/EN-symbols/sandwich.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 6,
     name: "Eat Dinner",
     image: "../mulberry-symbols/EN-symbols/dinner.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 7,
     name: "Play Inside",
     image: "../mulberry-symbols/EN-symbols/play_,_to.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 8,
     name: "Play Oustide",
     image: "../mulberry-symbols/EN-symbols/swing_,_to.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 9,
     name: "In-home Therapy",
     image: "../mulberry-symbols/EN-symbols/speech_language_therapist_1a.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 10,
     name: "Take Shower",
     image: "../mulberry-symbols/EN-symbols/shower_1_,_to.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 11,
     name: "Put on PJs",
     image: "../mulberry-symbols/EN-symbols/pyjamas.svg",
-    complete: false
+    complete: false,
+    checked: false
   },
   {
     taskId: 12,
     name: "Go to Bed",
     image: "../mulberry-symbols/EN-symbols/sleep_male_,_to.svg",
-    complete: false
+    complete: false,
+    checked: false
   }
 ]
 
